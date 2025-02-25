3012ca63c311a9113aecf55b095ff7fc43ca43e5
thekshitiz <dikhwachha@gmail.com>
2025-02-26 02:42:30 +0545

    updated html

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('webapp.urls')),
-
+    path('', views.home, name='home'),
 ]
